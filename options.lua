882e84663189c496c22e501e50e71f98afafe664
Samuel Dobroň <samuel@dobron.sk>
2023-09-02 13:13:53 +0200

    Disabled mode tooltip, adjusted tab size to 4

--- options.lua
+++ options.lua
@@ -7,6 +7,8 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    showmode = false,  -- disable showing modes in cmd line
+    tabstop = 4, -- Number of spaces in tab
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
